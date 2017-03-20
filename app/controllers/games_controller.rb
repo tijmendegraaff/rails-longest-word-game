3102347d48b1805be982cf3ee22c81a085127976
Tijmen de Graaff <tijmen76@gmail.com>
2017-03-20 17:04:32 +0100

    done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,77 @@
+class GamesController < ApplicationController
+  def game
+    @grid = generate_grid(9)
+    $start_time = Time.now
+  end
+
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+  end
+
+
+  def included?(answer, grid)
+    answer.split('').to_a.all? { |letter| answer.count(letter) <= grid.count(letter) }
+  end
+
+  def score_and_message(attempt, translation, grid, time)
+    if included?(attempt.upcase, grid)
+      if translation
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def get_translation(word)
+    api_key = "8208448d-370c-49d5-ad78-25f4ba3891b4"
+    begin
+      response = open("https://api-platform.systran.net/translation/text/translate?source=en&target=fr&key=#{api_key}&input=#{word}")
+      json = JSON.parse(response.read.to_s)
+      if json['outputs'] && json['outputs'][0] && json['outputs'][0]['output'] && json['outputs'][0]['output'] != word
+        return json['outputs'][0]['output']
+      end
+    rescue
+      if File.read('/usr/share/dict/words').upcase.split("\n").include? word.upcase
+        return word
+      else
+        return nil
+      end
+    end
+  end
+
+
+  def results
+    $time_taken = (Time.now - $start_time).to_s
+    @answer = params.fetch(:answer)
+    grid = params.fetch(:grid)
+    end_time = Time.now
+    @results = run_game(@answer, grid, $start_time, end_time)
+  end
+
+  def run_game(answer, grid, start_time, end_time)
+    result = { time: end_time - start_time }
+
+    result[:translation] = get_translation(@answer)
+    result[:score], result[:message] = score_and_message(
+      @answer, result[:translation], grid, result[:time])
+
+    result
+  end
+
+
+
+
+
+
+  def compute_score(attempt, time_taken)
+    (time_taken > 60.0) ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/game', to: 'games#game', as: :game
+
+  get 'results', to: 'games#results', as: :results
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
